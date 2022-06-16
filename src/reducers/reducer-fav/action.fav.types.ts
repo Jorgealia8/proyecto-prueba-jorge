1b6f1ebd2a8d07e5869c79419f2f2ce5d6011af9
Jorge Alia <jorgealia94@gmail.com>
2022-06-16 18:10:25 +0200

    Add characterReduceFav

---
+++ src/reducers/reducer-fav/action.fav.types.ts
@@ -0,0 +1,6 @@
+export enum actionTypes {
+    "characterFav@load" = "characterFav@load",
+    "characterFav@add" = "characterFav@add",
+    "characterFav@delete" = "characterFav@delete",
+    "characterFav@update" = "characterFav@update",
+}
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { CharacterModel } from "../data/characterModel";
+import { characterFavReducer } from "../reducers/reducer-fav/character.fav.reducer";
 import { characterReducer } from "../reducers/reducer-normal/character.reducer";
 
 const preloadedState = {
     characters: [] as Array<CharacterModel>,
+    charactersFav: [] as Array<CharacterModel>,
 };
 
 export const store = configureStore({
     reducer: {
         characters: characterReducer,
+        charactersFav: characterFavReducer,
     },
     preloadedState,
 });
